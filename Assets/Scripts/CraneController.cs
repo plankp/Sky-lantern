0026fab9db970d0be8c515b4ca2ed766cc414fe4
Paul Teng <plankp@outlook.com>
2019-10-14 09:09:23 -0400

    Split movement pattern from crane controller

--- Assets/Scripts/CraneController.cs
+++ Assets/Scripts/CraneController.cs
@@ -17,17 +17,6 @@ public class CraneController : MonoBehaviour
         }
     }
 
-    private float elapsed;
-
-    // Update is called once per frame
-    void Update()
-    {
-        this.elapsed += Time.deltaTime;
-
-        RotateBy(5 * Time.deltaTime);
-        TranslateBy(0.06f * Mathf.Sin(this.elapsed + Mathf.PI / 4));
-    }
-
     void OnDrawGizmos()
     {
         Gizmos.DrawRay(this.RotationPointWorld, Vector3.up * 10);
